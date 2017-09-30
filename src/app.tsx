a89e933680674b58a583f263aaa968c885644e82
Matthew Gonzalez <demure@gmail.com>
2017-09-30 15:26:45 -0700

    initial commit w/ basic electron app setup

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class App extends React.Component<undefined, undefined> {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React with Typescript! Test</h2>
+      </div>
+    );
+  }
+}
