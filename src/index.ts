db376ad9f6daffa5967031171047397eaef9ddf7
Matthew Gonzalez <demure@gmail.com>
2017-10-01 11:14:28 -0700

    added Info.plist and config to allow dragging files over app icon

--- src/index.ts
+++ src/index.ts
@@ -71,5 +71,7 @@ app.on('activate', () => {
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+app.on('open-file', function(event, pathToOpen) {
+	event.preventDefault();
+	console.log(pathToOpen);
+});
