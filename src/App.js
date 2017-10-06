a086457ae57a993aadc774a946dbfdc48276332a
Brian Gonzalez <me@briangonzalez.org>
2017-10-06 11:43:58 -0700

    initial site

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import appWindow from './fontplop.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="center">
+          <h1>The missing webfont converter for OSX.</h1>
+          <img src={appWindow} alt=""/>
+          <footer>
+            <a href="">Download</a>
+            <a href="">Donate</a>
+            <a href="">Contribute</a>
+          </footer>
+        </div>
       </div>
     );
   }
