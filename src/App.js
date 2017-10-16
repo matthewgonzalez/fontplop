b60144f9f4c17539d322a5ed0fbb4e04bea6c073
Brian Gonzalez <me@briangonzalez.org>
2017-10-16 13:09:24 -0700

    FontPlop -> fontplop

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ScreencastMp4 from './assets/fontplop-screencap.mp4'
 import ScreencastWebm from './assets/fontplop-screencap.webm'
 
 const github = 'https://www.github.com/matthewgonzalez/fontplop'
-const tweet = 'https://twitter.com/intent/tweet?text=I%20just%20downloaded%20FontPlop%20%E2%80%93%20the%20simple%2C%20fast%2C%20and%20free%20open%20source%20webfont%20converter%20for%20OSX%2FmacOS.%0A%0Ahttp%3A%2F%2Fwww.fontplop.com'
+const tweet = 'https://twitter.com/intent/tweet?text=I%20just%20downloaded%20fontplop%20%E2%80%93%20the%20simple%2C%20fast%2C%20and%20free%20open%20source%20webfont%20converter%20for%20OSX%2FmacOS.%0A%0Ahttp%3A%2F%2Fwww.fontplop.com'
 
 class App extends Component {
   render() {
@@ -25,7 +25,7 @@ class App extends Component {
             <a href={github}>Github</a>
           </div>
           <div id="what-is">
-            <p>FontPlop is an OSX/macOS application which takes <span className="filetype">ttf</span> and <span className="filetype">otf</span> files and outputs a webfont bundle: <span className="filetype">woff2</span>, <span className="filetype">woff</span>, <span className="filetype">tff</span>, <span className="filetype">svg</span>, and <span className="filetype">eot</span>.</p>
+            <p>fontplop is an OSX/macOS application which takes <span className="filetype">ttf</span> and <span className="filetype">otf</span> files and outputs a webfont bundle: <span className="filetype">woff2</span>, <span className="filetype">woff</span>, <span className="filetype">tff</span>, <span className="filetype">svg</span>, and <span className="filetype">eot</span>.</p>
             <p>It is the simpler, faster, free successor to <a href="#">FontPrep</a>, which is no longer maintained.</p>
           </div>
           <div id="crafted-by">
