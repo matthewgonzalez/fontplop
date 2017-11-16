f403907f599dde718527552125b8afc7a9cafdfd
matthewgonzalez <matthew.s.gonzalez@gmail.com>
2017-11-16 09:08:06 -0800

    files.forEach bug - Fixes #16

--- src/process-fonts.ts
+++ src/process-fonts.ts
@@ -2,6 +2,7 @@ import { FontCollection } from '../lib/font-collection'
 
 function processFonts (files: Array<any>) {
   if (!files) return
+  files = [].concat(files)
   files.forEach(file => {
     new FontCollection(file).export()
   })
